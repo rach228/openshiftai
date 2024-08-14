f7f0c4679d9abfa17395662c4e96afaf5b68dfff
Rachel Ma <130738524+rach228@users.noreply.github.com>
2024-08-14 15:13:17 +0800

    Update enrich_data.py

--- pipeline/enrich_data.py
+++ pipeline/enrich_data.py
@@ -1,20 +1,4 @@
 import os
-import boto3
-
-# Retrieve credentials and endpoint from environment variables
-key_id = os.getenv("AWS_ACCESS_KEY_ID")
-secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-endpoint = os.getenv("AWS_S3_ENDPOINT")
-bucket_name = os.getenv("AWS_S3_BUCKET")
-
-# Initialize S3 client
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=key_id,
-    aws_secret_access_key=secret_key,
-    endpoint_url=endpoint,
-    verify=False
-)
 
 # Printing the Environment Variables
 print('environment')
